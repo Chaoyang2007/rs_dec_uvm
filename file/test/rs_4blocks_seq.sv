1ee9ab17164cec5f064cd86378425f02c9f20c5d
scott <scott@scottx>
2024-01-08 23:39:46 -0800

    Backup, rs_4blocks_seq.sv and rs_utils.sv, with errors. Send tr first, inject errors, then calculate parity.

--- file/test/rs_4blocks_seq.sv
+++ file/test/rs_4blocks_seq.sv
@@ -1,14 +1,17 @@
-`ifndef RS_4BLOCKs_SEQ__SV
-`define RS_4BLOCKs_SEQ__SV
+`ifndef RS_4BLOCKS_SEQ__SV
+`define RS_4BLOCKS_SEQ__SV
 
 class rs_4blocks_seq extends uvm_sequence#(rs_transaction);
     `uvm_object_utils(rs_4blocks_seq)
     
-    rand bit has_error;
+    rand bit [3:0] has_error;
 
     constraint c {
         // soft has_error inside {0, 1};
-        soft has_error dist {0:=10, 1:=90};
+        soft has_error[3] dist {0:=30, 1:=70};
+        soft has_error[2] dist {0:=30, 1:=70};
+        soft has_error[1] dist {0:=30, 1:=70};
+        soft has_error[0] dist {0:=30, 1:=70};
     }
 
     function new(string name="rs_4blocks_seq");
@@ -16,8 +19,8 @@ class rs_4blocks_seq extends uvm_sequence#(rs_transaction);
     endfunction // new()
 
     virtual task body();
-        rs_transaction tr, drv_tr;
-        bit [1551:0] buffer = 0; //194B
+        rs_transaction tr;
+        bit [194`B:0] buffer = 0; //194B
         bit [31:0]   parity = 0;
         integer      count  = 0;
 
@@ -29,14 +32,14 @@ class rs_4blocks_seq extends uvm_sequence#(rs_transaction);
             assert(tr.randomize());
             if (tr.rx_vld) begin
                 if (count < 2 * `SYMBOLS_IN_BLOCK) begin
-                    buffer = {buffer[1487:0],tr.rx_data}; // all buffered
+                    buffer = {buffer[186`B:0],tr.rx_data}; // all buffered
                     count  = count + 1;
                 end else begin
                     tr.rx_data[63:60] = 0;
-                    buffer = {buffer[1535:0],tr.rx_data[63:48]}; // 2B syncbit buffered
-                    rs_utils::calculate_par(buffer,has_error,parity);
+                    buffer = {buffer[192`B:0],tr.rx_data[63:48]}; // 2B syncbit buffered
+                    rs_utils::parity_cal_with_err(buffer,has_error[3],parity);
                     tr.rx_data[47:16] = parity;
-                    buffer = {buffer[1535:0],tr.rx_data[15:0]}; // 2B next buffered
+                    buffer = {buffer[192`B:0],tr.rx_data[15:0]}; // 2B next buffered
                     count  = 0;
                     finish_item(tr);
                     break;
@@ -53,16 +56,16 @@ class rs_4blocks_seq extends uvm_sequence#(rs_transaction);
             assert(tr.randomize());
             if (tr.rx_vld) begin
                 if (count < 2 * `SYMBOLS_IN_BLOCK - 1) begin
-                    buffer = {buffer[1487:0],tr.rx_data}; // all buffered
+                    buffer = {buffer[186`B:0],tr.rx_data}; // all buffered
                     count  = count + 1;
                 end else if (count == 2 * `SYMBOLS_IN_BLOCK - 1) begin
                     tr.rx_data[15:12] = 0;
-                    buffer = {buffer[1487:0],tr.rx_data}; // all buffered with 2B syncbit
-                    rs_utils::calculate_par(buffer,has_error,parity);
+                    buffer = {buffer[186`B:0],tr.rx_data}; // all buffered with 2B syncbit
+                    rs_utils::parity_cal_with_err(buffer,has_error[2],parity);
                     count  = count + 1;
                 end else begin
                     tr.rx_data[63:32] = parity;
-                    buffer = {buffer[1519:0],tr.rx_data[31:0]}; // 4B next buffered
+                    buffer = {buffer[190`B:0],tr.rx_data[31:0]}; // 4B next buffered
                     count  = 0;
                     finish_item(tr);
                     break;
@@ -79,17 +82,17 @@ class rs_4blocks_seq extends uvm_sequence#(rs_transaction);
             assert(tr.randomize());
             if (tr.rx_vld) begin
                 if (count < 2 * `SYMBOLS_IN_BLOCK - 1) begin
-                    buffer = {buffer[1487:0],tr.rx_data}; // all buffered
+                    buffer = {buffer[186`B:0],tr.rx_data}; // all buffered
                     count  = count + 1;
                 end else if (count == 2 * `SYMBOLS_IN_BLOCK - 1) begin
                     tr.rx_data[31:28] = 0;
-                    buffer = {buffer[1503:0],tr.rx_data[63:16]}; // 6B buffered with 2B syncbit
-                    rs_utils::calculate_par(buffer,has_error,parity);
+                    buffer = {buffer[188`B:0],tr.rx_data[63:16]}; // 6B buffered with 2B syncbit
+                    rs_utils::parity_cal_with_err(buffer,has_error[1],parity);
                     tr.rx_data[15:0] = parity[31:16];
                     count  = count + 1;
                 end else begin
                     tr.rx_data[63:48] = parity[15:0];
-                    buffer = {buffer[1503:0],tr.rx_data[63:16]}; // 6B next buffered
+                    buffer = {buffer[188`B:0],tr.rx_data[47:0]}; // 6B next buffered
                     count  = 0;
                     finish_item(tr);
                     break;
@@ -106,12 +109,12 @@ class rs_4blocks_seq extends uvm_sequence#(rs_transaction);
             assert(tr.randomize());
             if (tr.rx_vld) begin
                 if (count < 2 * `SYMBOLS_IN_BLOCK - 1) begin
-                    buffer = {buffer[1487:0],tr.rx_data}; // all buffered
+                    buffer = {buffer[186`B:0],tr.rx_data}; // all buffered
                     count  = count + 1;
                 end else begin
                     tr.rx_data[47:44] = 0;
-                    buffer = {buffer[1519:0],tr.rx_data[63:32]}; // 4B buffered with 2B syncbit
-                    rs_utils::calculate_par(buffer,has_error,parity);
+                    buffer = {buffer[190`B:0],tr.rx_data[63:32]}; // 4B buffered with 2B syncbit
+                    rs_utils::parity_cal_with_err(buffer,has_error[0],parity);
                     tr.rx_data[32:0] = parity;
                     count  = 0;
                     finish_item(tr);
@@ -127,4 +130,4 @@ class rs_4blocks_seq extends uvm_sequence#(rs_transaction);
 
 endclass //rs_4blocks_seq extends uvm_sequence#(rs_transaction)
 
-`endif // RS_4BLOCKs_SEQ__SV
+`endif // RS_4BLOCKS_SEQ__SV
--- file/uvm_list.f
+++ file/uvm_list.f
@@ -15,6 +15,8 @@
 
 ../file/test/rs_vsqr.sv
 ../file/test/rs_4blocks_seq.sv
+../file/test/rs_4blocks_tb_seq.sv
+../file/test/rs_4blocks_tr_seq.sv
 ../file/test/rs_base_vseq.sv
 ../file/test/rs_error_case_vseq.sv
 ../file/test/rs_base_test.sv
